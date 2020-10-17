029cda9184fac1e9ea5ed0d42b95b28554126fbf
Max Mönch <max.moench@hexfourty.com>
2020-10-17 19:22:40 +0200

    addedHTMLSanitation

--- src/shared/mail.ts
+++ src/shared/mail.ts
@@ -1,3 +1,5 @@
+import sanitizeHtml from 'sanitize-html';
+
 export interface IMail {
   from: string;
   to: string;
@@ -19,7 +21,11 @@ export class Mail implements IMail {
     this.to = rawMail.to.text;
     this.from = rawMail.from.text;
     this.body = rawMail.text;
-    this.htmlBody = rawMail.html;
+    try {
+      this.htmlBody = sanitizeHtml(rawMail.html);
+    } catch (error) {
+      this.htmlBody = rawMail.text;
+    }
     this.subject = rawMail.subject;
     this.recived = rawMail.date;
   }
